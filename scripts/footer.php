e4ff2656c2ac62de40e9b36c8ee30595077a6a6a
Ivone Corleto <icorleto355@alumno.unlam.edu.ar>
2024-09-17 01:17:56 -0300

    Agrego archivos del proyecto al repo

---
+++ scripts/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabajo Práctico</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="/Pokedex/style/footer.css">
+</head>
+<body>
+
+
+<div class="w3-container footer">
+    <h3 style="color: black ; text-align: left">Programacion Web II 2024</h3>
+    <h3>Alan Dominguez</h3>
+    <h3>Guido Sersewitz</h3>
+    <h3>Julian Suarez</h3>
+    <h3>Ivone Corleto</h3>
+</div>
+</body>
+</html>
